fcd86e9aefbe6e01bd2520dd0af5f61e10b562c4
Araaf Royall ❂ <swordigoar@gmail.com>
2026-05-24 20:10:16 +0530

    Update ExtraMenuBean.java

--- app/src/main/java/pro/sketchware/menu/ExtraMenuBean.java
+++ app/src/main/java/pro/sketchware/menu/ExtraMenuBean.java
@@ -14,6 +14,7 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 import java.util.LinkedHashSet;
 import androidx.annotation.NonNull;
+import android.text.TextUtils;
 
 import com.besome.sketch.beans.AdTestDeviceBean;
 import com.besome.sketch.beans.AdUnitBean;
